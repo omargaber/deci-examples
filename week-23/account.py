90f31a2e9d94657b40c4da2d54b0993b8cf7f4a6
Omar <omargaber956@gmail.com>
2023-04-13 02:03:33 +0200

    Fixes.

--- week-23/account.py
+++ week-23/account.py
@@ -1,109 +1,119 @@
 import random
 import time
 
+
 def validate_input(value):
     try:
-        if eval(value):
-            return True, eval(value)
+        if float(value):
+            return True, float(value)
+        
+        return False, float(value)
     except:
-        print("ECC")
-        print(value, type(value))
+        print("EXCEPT")
+        print("ECEEPTIONN", value, type(value))
         return False, str(value)
+
+
+def processing_sleep():
+    print("=======Processing=======")
+    time.sleep(2)
 
 
 def banking_operations(account):
     while True:
-        operation_message = "Please pick an operation you want to perform on your account.\n1- Account Details.\n2- Deposit.\n3- Withdraw.\n4- Exit.\n"
+        operation_message = "Please pick an operation you want to perform on your account.\n1- Account Details.\n2- Deposit.\n3- Withdraw.\n4- Exit.\nPlease input the action number: "
         operation_action, opereation_value = validate_input(input(operation_message))
 
-        while not operation_action or operation_action not in [1,2,3,4]:
-            operation_action, opereation_value = validate_input(input(f"Invalid input.\m{operation_message}"))
+        while not operation_action or operation_action not in [1, 2, 3, 4]:
+            operation_action, opereation_value = validate_input(
+                input(f"Invalid input.\m{operation_message}")
+            )
+
+        processing_sleep()
 
         if opereation_value == 4:
-            break
+            return
 
         if opereation_value == 1:
-            account.details()
+            print(account.details())
 
         if opereation_value == 2:
             prompt_message = "Please input the amount you want to deposit: "
             deposit_flag, deposit_amount = validate_input(input(prompt_message))
 
             while not deposit_flag or deposit_amount <= 0:
-                deposit_flag, deposit_amount = validate_input(input(f"Invalid input.\n{prompt_message}"))
+                deposit_flag, deposit_amount = validate_input(
+                    input(f"Invalid input.\n{prompt_message}")
+                )
 
             account.deposit(deposit_amount)
-            
-        
+
         if opereation_value == 3:
-            prompt_message = "Please input the amount you want to withdraw: "
-            deposit_flag, withdraw_amount = validate_input(input(prompt_message))
+            account.withdraw()
 
-            while not deposit_flag or withdraw_amount <= 0:
-                deposit_flag, deposit_amount = validate_input(input(f"Invalid input.\n{prompt_message}"))
+        if opereation_value != 1:
+            processing_sleep()
+            print(account.details())
+        print("----------------------------------------------------")
+        time.sleep(2)
 
-            account.withdraw(withdraw_amount)
-        
-        print(account.details())
-        
-    
+
 class Account:
-    
     name: str = None
     email: str = None
     balance: float = 0
-    
+
     def __init__(self, name, email, balance=0):
         self.name = name
         self.email = email
         self.balance = balance
-        
+
         print(self.details())
-    
+
     def details(self):
-        return {
-            "name": self.name,
-            "email": self.email,
-            "balance": self.balance
-        }
-        
-    
+        print(
+            "========================= Here are your account details ========================="
+        )
+        return {"name": self.name, "email": self.email, "balance": self.balance}
+
     def deposit(self, amount):
         self.balance += amount
         return True
-    
-    def withdraw(self, amount):
-        flag, value = validate_input(amount)
-        while not flag or value <= 0:
-            flag, value = validate_input(input("Invalid input. Please input the amount you want to withdraw: "))
-        
-        while not flag or value > self.balance:
-            flag, value = validate_input(input(f"Insufficient funds. Please try amounts <= {self.balance}: "))
-        
+
+    def withdraw(self):
+        prompt_message = "Please input the amount you want to withdraw: "
+        flag, value = validate_input(input(prompt_message))
+        while not flag or value <= 0 or value > self.balance:
+            if not flag or value <= 0:
+                flag, value = validate_input(input("Invalid input. {prompt_message}"))
+            else:
+                flag, value = validate_input(
+                    input(f"Insufficient funds. Please try amounts <= {self.balance}: ")
+                )
+
         self.balance -= value
         return True, self.balance
-    
 
-banks = [
-    "HSBC",
-    "NBE",
-    "CIB"
-]
 
-main_prompt = "Kidnly choose from the following actions:\n1- Create an account.\n2- Exit.\n"
+banks = ["HSBC", "NBE", "CIB"]
+
+main_prompt = (
+    "Kidnly choose from the following actions:\n1- Create an account.\n2- Exit.\n"
+)
+
+
 def main():
     bank_name = random.choice(banks)
     print(f"Welcome to {bank_name} banking services.")
     time.sleep(2)
 
     input_flag, value = validate_input(input(main_prompt))
-    while not input_flag or value not in [1,2]:
+    while not input_flag or value not in [1, 2]:
         input_flag, value = validate_input(input(f"Invalid input.\n{main_prompt}"))
 
     if value == 2:
         print(f"Thank you for using {bank_name} banking services.")
         return
-
 
     time.sleep(2)
     name = input("Please enter your name: ")
@@ -113,24 +123,21 @@ def main():
 
     email = input("Please enter your email address: ")
     while len(email) == 0:
-        email = input(
-            "Please enter a valid name, should have at least one character: "
-        )
-    
-    
-    balance_flag, balance_amount = validate_input(input("Please input your initial balance: "))
-    while not balance_flag or balance_amount <= 0:
-        balance_flag, balance_amount = validate_input(input("Please input a valid amount more than 0: "))
-        
-    created_account = Account(
-        name=name,
-        email=email,
-        balance=balance_amount
+        email = input("Please enter a valid name, should have at least one character: ")
+
+    balance_flag, balance_amount = validate_input(
+        input("Please input your initial balance: ")
     )
-    
+    while not balance_flag or balance_amount <= 0:
+        balance_flag, balance_amount = validate_input(
+            input("Please input a valid amount more than 0: ")
+        )
+
+    created_account = Account(name=name, email=email, balance=balance_amount)
+
     banking_operations(created_account)
+    print(f"Thank you for using {bank_name} banking services.")
     return
 
 
-main()    
-    
+main()
