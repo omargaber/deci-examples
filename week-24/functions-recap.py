7ece515305d544d21d079556396017047752a9da
Ali Helmi <ahelmi365@gmail.com>
2023-06-23 11:36:17 +0300

    updated function names to start with verb

--- week-24/functions-recap.py
+++ week-24/functions-recap.py
@@ -2,6 +2,14 @@
 # with exercises for the students that you will prompt them
 # to answer during the session
 
+# 1. What is a function?
+#  - A function is a block of code that performs a specific task.
+#  - It is a reusable piece of code that can be called multiple times.
+#  - It can take parameters and return values.
+#  - It can be used to organize code into logical blocks.
+#  - It can be used to avoid repetition of code.
+#  - It can be used to make code easier to understand.
+
 
 # FUNCION COMPONENTS
 # Function name
@@ -10,44 +18,44 @@
 # Function return
 
 
-def adder(x, y):
+def add_wo_numbers(x, y):
     value = x+y
     return value
 
 # Also
-# def adder(x, y):
+# def add_wo_numbers(x, y):
 #     return x+y
 
-x = adder(3, 4)
+x = add_wo_numbers(3, 4)
 
 # For the above example, we can identify the components of the function
-# Function name: adder
+# Function name: add_wo_numbers
 # Function parameters: takes two arguments upon function call: x and y (BOTH SHOULD BE NUMERIC VALUES)
 # Function logic: Adds the arguments passed together
 # Function return: Expects to return one value (summation of passed arguments)
 
 
 
-def average(numbers):
+def get_average(numbers):
     return sum(numbers) / len(numbers)
 
 # For the above example, we can identify the components of the function
-# Function name: average
+# Function name: get_average
 # Function parameters: takes one argument upon function call: numbers (SHOULD BE A LIST OF NUMERIC VALUES)
 # Function logic: Returns the average by summing the list elements and dividing them by the number of elements.
 # Function return: Expects to return one value (average of passed arguments)
 
-x = average([1,2,3,4,5,6])
+x = get_average([1,2,3,4,5,6])
 
 
 
 
 # Multi value return function
-def square_numbers_passed(x,y):
+def square_two_numbers(x,y):
     return x**2, y**2
 
 # For the above example, we can identify the components of the function
-# Function name: square_numbers_passed
+# Function name: square_two_numbers
 # Function parameters: akes two arguments upon function call: x and y (BOTH SHOULD BE NUMERIC VALUES)
 # Function logic: Returns the square of the arguments passed to the function
 # Function return: Expects to return TWO values (square of both numebrs)
@@ -55,7 +63,7 @@ def square_numbers_passed(x,y):
 
 # For multi-value return functions show them students how the function should be called
 
-num1, num2 = square_numbers_passed(2,3)
+num1, num2 = square_two_numbers(2,3)
 # Notice here that the function call that has 2 returns needs to have values stored in 
 # variables with the same number of return values
 
@@ -65,7 +73,7 @@ num1, num2 = square_numbers_passed(2,3)
 
 # Exercises drive by students participation
 
-def longest_string(strings):
+def get_longest_string(strings):
     longest_string = ""
     for string in strings:
         if len(string) > len(longest_string):
@@ -86,4 +94,4 @@ def longest_string(strings):
 # Function parameters: takes one argument upon function call: strings (SHOULD BE A LIST OF STRINGS)
 # Function logic: Stores the longest string from the passed arguments in a variable to return the longest string
 # Function return: Returns one value that is of string format
-x = longest_string(["THIS IS", "THE LONGEST STRING", "Hello"])
+x = get_longest_string(["THIS IS", "THE LONGEST STRING", "Hello"])
